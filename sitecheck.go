3743ac18eb1b74ae99f470ad33c67960209c6c36
tenkoh <tenkoh.go@gmail.com>
2023-05-18 05:47:42 +0900

    sitecheck: add UpdateModificationRecord, then change IsModified into private function (isModified). Testability of IsModified is covered in UpdateModificationRecords's test.

--- sitecheck.go
+++ sitecheck.go
@@ -13,13 +13,34 @@ type Repository interface{}
 // Crawler is an interface for crawling sites.
 type Crawler interface{}
 
-// IsModified returns true if the modified date is newer than any of the existing modified dates.
+// isModified returns true if the modified date is newer than any of the existing modified dates.
 // When there is no existing modified date, it returns true.
-func IsModified(exists []time.Time, modified time.Time) bool {
+func isModified(exists []time.Time, modified time.Time) bool {
 	for _, t := range exists {
 		if t.Compare(modified) >= 0 {
 			return false
 		}
 	}
 	return true
+}
+
+// UpdateModificationRecord returns a new modification record updated
+// with the given URL and modified date.
+func UpdateModificationRecord(
+	exists ModificationRecord,
+	url string,
+	modified time.Time,
+) ModificationRecord {
+	m := ModificationRecord{}
+	for k, v := range exists {
+		m[k] = v
+	}
+	if ms, ok := m[url]; ok {
+		if isModified(ms, modified) {
+			m[url] = append(ms, modified)
+		}
+	} else {
+		m[url] = []time.Time{modified}
+	}
+	return m
 }
--- sitecheck_test.go
+++ sitecheck_test.go
@@ -1,6 +1,7 @@
 package sitecheck_test
 
 import (
+	"reflect"
 	"testing"
 	"time"
 
@@ -9,31 +10,78 @@ import (
 
 /*
 TODO:
-ok 1. 既存の更新日時一覧と、新たに取得した更新日時を比較する。
-2. 更新日時が既存の更新日時のどれよりも新しい場合、更新日時を追加して保存する。
-3. 更新日時が既存の更新日時のどれよりも古い場合、何もしない。
+1. 既存の更新日時一覧と、新たに取得した更新日時を比較する。
+-> ok
+-> 2で網羅可能なためリファクタリングで削除しプライベート関数にする
+
+2. 比較結果に応じて既存の更新日時一覧を更新する。
+  - 新たに取得した更新日時が既存の更新日時のどれよりも新しい場合、既存の更新日時一覧に追加する。
+  - 新たに取得した更新日時が既存の更新日時のいずれかよりも古い場合、何もしない。
+-> ok
 */
 
-func TestIsModified(t *testing.T) {
+func TestUpdateModificationRecord(t *testing.T) {
 	t1 := time.Date(2020, 1, 1, 0, 0, 0, 0, time.UTC)
 	tests := []struct {
 		name     string
-		exists   []time.Time
+		record   sitecheck.ModificationRecord
+		url      string
 		modified time.Time
-		want     bool
+		want     sitecheck.ModificationRecord
 	}{
-		{"modified is after existing dates", []time.Time{t1}, t1.Add(1 * time.Second), true},
-		{"modified is equal to one of existing dates", []time.Time{t1}, t1, false},
-		{"modified is before existing dates", []time.Time{t1}, t1.Add(-1 * time.Second), false},
-		{"empty input", []time.Time{}, t1, true},
+		{
+			"add new modified date",
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1}},
+			"https://example.com",
+			t1.Add(1 * time.Second),
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1, t1.Add(1 * time.Second)}},
+		},
+		{
+			"no update when modified date is older",
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1}},
+			"https://example.com",
+			t1.Add(-1 * time.Second),
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1}},
+		},
+		{
+			"no update when modified date is equal to one of existing dates",
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1}},
+			"https://example.com",
+			t1,
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1}},
+		},
+		{
+			"add new modified date when there is no existing date for the URL",
+			sitecheck.ModificationRecord{"https://example1.com": []time.Time{t1}},
+			"https://example.com",
+			t1,
+			sitecheck.ModificationRecord{
+				"https://example1.com": []time.Time{t1},
+				"https://example.com":  []time.Time{t1},
+			},
+		},
+		{
+			"add new modified date when exists is empty",
+			sitecheck.ModificationRecord{},
+			"https://example.com",
+			t1,
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1}},
+		},
+		{
+			"add new modified date when the key exists but the value is empty",
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{}},
+			"https://example.com",
+			t1,
+			sitecheck.ModificationRecord{"https://example.com": []time.Time{t1}},
+		},
 	}
 
 	t.Parallel()
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := sitecheck.IsModified(tt.exists, tt.modified)
-			if got != tt.want {
-				t.Errorf("isModified() = %t, want %t", got, tt.want)
+			got := sitecheck.UpdateModificationRecord(tt.record, tt.url, tt.modified)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("UpdateModificationRecord() = %v, want %v", got, tt.want)
 			}
 		})
 	}
